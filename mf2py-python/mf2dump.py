6d5d8306c2d92ff38bbc3cef53c2bf6b58494b7e
Johannes Ernst <jernst@indiecomputing.com>
2018-02-27 21:25:50 +0000

    Fix dependencies Allow URLs and local files

--- mf2py-python/mf2dump.py
+++ mf2py-python/mf2dump.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 
+import argparse
 import json
 from mf2py import Parser
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit( 'Usage: %s <url>' % sys.argv[0] )
+parser = argparse.ArgumentParser(description='Emit JSON that contains the recognized microformats in an HTML page')
+parser.add_argument('--stdin', action='store_true', help='If provided, read from standard input')
+parser.add_argument('url', nargs='*', help='If not reading from stdin, read from this URL')
 
-url = sys.argv[1]
+args = parser.parse_args()
 
-p = Parser( None, url, 'lxml' )
+content=None
+
+if args.stdin:
+  if args.url:
+    sys.exit('ERROR: Either give --stdin or <url>, not both' )
+  content = sys.stdin.read()
+else:
+  if len(args.url) != 1:
+    sys.exit('ERROR: Must provide a URL or --stdin')
+
+p = Parser( content, None if len(args.url) == 0 else args.url[0], 'lxml' )
 obj = p.to_dict()
 
 print( json.dumps(obj, indent=4, sort_keys=True))
